1ce2124f9dd06ac9d2a2ead83a990f5b11e998e6
Steve Roger <stranger0411@gmail.com>
2018-08-17 21:37:50 +1000

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+/// <reference types="@types/googlemaps" />
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAK8MrWHM4riyD4HJ6M5pabVAs09scuLBU'
+    })
+  ],
+  providers: [],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
+
